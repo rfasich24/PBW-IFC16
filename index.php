001801f8fe090e5c323928fa45e0312fa10b26b2
rfasich24 <202410103065@mail.unej.ac.id>
2022-06-15 00:05:42 +0700

    Fitur Read 2.0
    Fitur Read 2.0

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
     <link rel="stylesheet" href="Model.css" />
-    <title>Document</title>
+    <title>Project Akhir - Northwind</title>
 </head>
 
 <body style="background-color: #ffa500">
@@ -16,7 +16,7 @@
             <h1 class="judul"><b>Northwind</b></h1>
             <div class="menu" style="color: white">
                 <h5 style="display: inline; margin-right: 4em">
-                    <button class="menu" href="#">Home</button>
+                    <button class="menu"><a class="menu" href="index.html">Home</a></button>
                 </h5>
                 <h5 style="display: inline; margin-right: 4em">
                     <button class="menu" href="#">Order</button>
@@ -28,9 +28,26 @@
         </div>
     </nav>
     <section>
-        <div class="container mt-5">
+        <div class="container mt-4">
+            <table>
+                <tr>
+                    <td>
+                        <button class="bi"><svg xmlns="http://www.w3.org/2000/svg" width="60" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z" />
+                            </svg></button>
+                    </td>
+                    <td>
+                        <div class="search">
+                            <div class="input-group">
+                                <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+                                <button type="button" class="btn btn-outline-primary">search</button>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
             <!--CARD-->
-            <div class="row justify-content-center" id="data">
+            <div class="row justify-content-center mt-4" id="data">
 
             </div>
             <!--Isi Film dan tombol lebih banyak-->
@@ -46,7 +63,6 @@
         </div>
     </section>
 
-
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
@@ -56,21 +72,21 @@
         $(document).ready(function() {
             $("#more").click(function() {
                 $(this).html("Loading...").attr("disabled", "disabled");
-                $.get("db.php", function(response) {
+                $.get("db.php?begin=" + page, function(response) {
                     $.each(response, function(key, value) {
                         $("#data").append(
-                            "<div class='col-md-3 mb-4'>" +
+                            "<div class='col-md-3 mb-4'>" + "<a href='detail.php?film_id= >" + value.ProductID + "'>" +
                             "<div class='card'>" +
-                            "<img src='https://static.wikia.nocookie.net/genshin-impact/images/a/ad/Item_Tofu.png/revision/latest?cb=20220521075420&path-prefix=id' alt='Gambar"+value.ProductName+"' />" +
+                            "<img src='https://static.wikia.nocookie.net/genshin-impact/images/a/ad/Item_Tofu.png/revision/latest?cb=20220521075420&path-prefix=id' alt='Gambar" + value.ProductName + "' />" +
                             "<div class='container'>" +
                             "<div class='card-body'>" +
                             "<div class='row'>" +
-                            "<div class='col-9 ms-0'>" +
-                            "<h4 class='card-title'> <a class='title' href='details.php?id=" + value.ProductID + "'>"+value.ProductName+ "</a></h4></div>" +
+                            "<div class='col-14 ms-0'>" +
+                            "<h4 class='card-title'>" + value.ProductName + "</div>" +
                             "<div class='col-8 ms-0'>" +
-                            "<h6 class='card-title'>Stock : "+value.UnitsInStock+ "</h6></div>" +
+                            "<h6 class='card-info'>Stock : " + value.UnitsInStock + "</h6></div>" +
                             "<div class='col-8 ms-0'>" +
-                            "<h6 class='card-title'>Price : "+value.UnitPrice+ "</h6>" +
+                            "<h6 class='card-info'>Price : " + value.UnitPrice + "</h6>" +
                             "</div></div></div></div></div></div>"
                         );
                     });
